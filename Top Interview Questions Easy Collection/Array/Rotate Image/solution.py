e22816c60cb05774b3cc9a286d8ffafd34de9c78
zhweiliu <zhweiliu@gmail.com>
2021-03-18 03:46:53 +0800

    Add description to each *.py files for the part of Array question.

--- Top Interview Questions Easy Collection/Array/Rotate Image/solution.py
+++ Top Interview Questions Easy Collection/Array/Rotate Image/solution.py
@@ -1,29 +1,76 @@
 from typing import List
 
+'''
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. 
+DO NOT allocate another 2D matrix and do the rotation.
+
+Example 1:
+Input: matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+Output: [
+    [7,4,1],
+    [8,5,2],
+    [9,6,3]
+]
+
+
+Example 2:
+Input: matrix = [
+    [5,1,9,11],
+    [2,4,8,10],
+    [13,3,6,7],
+    [15,14,12,16]
+]
+Output: [
+    [15,13,2,5],
+    [14,3,4,1],
+    [12,6,8,9],
+    [16,7,10,11]
+]
+
+Example 3:
+Input: matrix = [[1]]
+Output: [[1]]
+
+'''
+
+
 def reverse(nums: List, start: int, end: int) -> None:
     while start < end:
         nums[start], nums[end] = nums[end], nums[start]
-        start, end = start+1, end-1
+        start, end = start + 1, end - 1
+
 
 def transpost(matrix: List[List[int]]) -> None:
     for i in range(len(matrix)):
-        for j in range(i+1, len(matrix), 1):
+        for j in range(i + 1, len(matrix), 1):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
+
 def rotate(matrix: List[List[int]]) -> None:
+    """
+    divide to 2 parts for right rotate 90 degree for matrix
+    1. transpost elements by diagonal
+    2. reverse each row
+    :param matrix:
+    :return:
+    """
     transpost(matrix)
     for row in matrix:
-        reverse(row, 0, len(row)-1)
-
-
+        reverse(row, 0, len(row) - 1)
 
 
 if __name__ == '__main__':
     matrix = [
-        [5,1,9,11],
-        [2,4,8,10],
-        [13,3,6,7],
-        [15,14,12,16]
+        [5, 1, 9, 11],
+        [2, 4, 8, 10],
+        [13, 3, 6, 7],
+        [15, 14, 12, 16]
     ]
 
     rotate(matrix)
--- Top Interview Questions Easy Collection/Array/Valid Sudoku/solution.py
+++ Top Interview Questions Easy Collection/Array/Valid Sudoku/solution.py
@@ -1,5 +1,48 @@
 from typing import List, Dict
 
+'''
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+Note:
+    - A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+    - Only the filled cells need to be validated according to the mentioned rules.
+
+
+Example 1:
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+
+
+Example 2:
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. 
+Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+'''
+
 
 def isValidSudoku(board: List[List[str]]) -> bool:
     row_h: List[Dict] = [{}, {}, {}, {}, {}, {}, {}, {}, {}]
@@ -8,7 +51,7 @@ def isValidSudoku(board: List[List[str]]) -> bool:
 
     for row_idx in range(9):
         for col_idx in range(9):
-            cube_idx = int(row_idx/3) * 3 + int(col_idx/3)
+            cube_idx = int(row_idx / 3) * 3 + int(col_idx / 3)
 
             num = board[row_idx][col_idx]
 
@@ -23,6 +66,7 @@ def isValidSudoku(board: List[List[str]]) -> bool:
             cube_h[cube_idx][num] = cube_idx
 
     return True
+
 
 if __name__ == '__main__':
     board = [
